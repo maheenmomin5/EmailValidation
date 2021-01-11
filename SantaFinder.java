c5efbbdbc4db70f8d8ed0c82858653bee122b3f2
maheenmomin5 <76459963+maheenmomin5@users.noreply.github.com>
2021-01-11 18:15:03 +0000

    Add files via upload

---
+++ SantaFinder.java
@@ -0,0 +1,38 @@
+package find_santa;
+
+import java.util.Scanner;
+
+public class SantaFinder {
+	
+	public static void main(String[] args) {
+		
+		SantaSuspects santaSuspects = new SantaSuspects (20);
+		
+				santaSuspects.addSuspect("bob@bezanga.com"); 
+				santaSuspects.addSuspect("sally@zimmers.com"); 
+		 		santaSuspects.addSuspect("peter@pinkelton.com"); 
+		 		santaSuspects.addSuspect("bobby.drop@tables.com"); 
+		 		santaSuspects.addSuspect("rudolph@rednose.com"); 
+		 		santaSuspects.addSuspect("bob@ross.com"); 
+		 		santaSuspects.addSuspect("ubiga@altman.com"); 
+		 		santaSuspects.addSuspect("tony@ross.com"); 
+		 		santaSuspects.addSuspect("aaron@rogers.com"); 
+		 		santaSuspects.addSuspect("yolo@once.com"); 
+		 		santaSuspects.addSuspect("yoka@yamalla.com"); 
+		 		santaSuspects.addSuspect("big@show.com"); 
+		 		santaSuspects.addSuspect("boug@dimmadome.com"); 
+
+		 		System.out.println("Provide a name to search for:");
+		 		Scanner scanner = new Scanner (System.in);
+		 		String searchFor = scanner.nextLine();
+		 		
+		 		boolean foundMatch = santaSuspects.foundMatch(searchFor);
+		 		if (foundMatch) {
+		 			System.out.println(searchFor + " is indeed a santa suspect");
+		 			
+		 		} else {
+		 			System.out.println(searchFor + " is not a santa suspect");
+		 			
+		 		}
+	}
+}
